d73fff931e490d02e6897f2e0ea83f4c9dc285d9
Pratibha Agrawal <125815236+pratibha12-34@users.noreply.github.com>
2025-05-14 22:17:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const parseCode = (str) => {
-  // your code here
+	let sep_r=str.split('0')
+	console.log(sep_r)
+	// return {} 
 };
 
 // Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+// const str = prompt("Enter str: ");
+// alert(JSON.stringify(parseCode(str)));
+parseCode("John000Doe0000123")
